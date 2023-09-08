08277dc3cc621a68bfd7e21fdcd953a5d27618f2
consul <2301488793@qq.com>
2023-09-08 09:11:18 +0800

    写用slice，添加消费线程，解决处理数据速度慢，导致丢失数据问题

--- app/src/main/java/com/practice/serialportpractice/SerialPortHelper.java
+++ app/src/main/java/com/practice/serialportpractice/SerialPortHelper.java
@@ -7,8 +7,11 @@ import java.io.File;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedTransferQueue;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
@@ -25,10 +28,10 @@ public class SerialPortHelper {
     private InputStream mFileInputStream;
     private ReadThread readThread;
     private WriteThread writeThread;
+    private ConsumeThread consumeThread;
     private OutputStream mFileOutputStream;
     //READ
     private byte[] buffer = new byte[1024];
-    private ByteBuf readPool = Unpooled.buffer();
     boolean stopRead = false;
     boolean skipReadWait = false;
     ReentrantLock readLock = new ReentrantLock();
@@ -40,6 +43,14 @@ public class SerialPortHelper {
     Condition writeCondition = writeLock.newCondition();
     ReentrantLock sleepLock = new ReentrantLock();
     Condition sleepCondition = sleepLock.newCondition();
+    //CONSUME
+    boolean stopConsume = false;
+    LinkedList<ConsumeData> consumeDatas = new LinkedList<>();
+    ReentrantLock consumeLock = new ReentrantLock();
+    Condition consumeCondition = consumeLock.newCondition();
+    private ByteBuf readPool = Unpooled.buffer();
+
+
     private Cmd cmdToSend = null;
     private byte[] bytesToSend = null;
 
@@ -55,31 +66,94 @@ public class SerialPortHelper {
     Callback callback;
 
     private void stopWrite() {
-        sleepLock.lock();
+        try {
+            sleepLock.lock();
+        } catch (Exception e) {
+
+        }
         stopWrite = true;
-        sleepCondition.signalAll();
-        sleepLock.unlock();
+        try {
+            sleepCondition.signalAll();
+        } catch (Exception e) {
+
+        }
+        try {
+            sleepLock.unlock();
+        } catch (Exception e) {
+        }
         sendCmd(new Cmd.Builder().data(Unpooled.buffer()).clearQueue(true).interval(0).extra("").build());
     }
 
-    private void stopRead() {
-        readLock.lock();
-        stopRead = true;
-        readCondition.signalAll();
-        readLock.unlock();
+    private void stopConsume() {
+        stopConsume = true;
+        submitConsumeData(new ConsumeData.Builder().exitSignal(true).build());
     }
+
+    private void stopRead() {
+        try {
+            readLock.lock();
+        } catch (Exception e) {
+
+        }
+        stopRead = true;
+        try {
+            readCondition.signalAll();
+        } catch (Exception e) {
+
+        }
+        try {
+            readLock.unlock();
+        } catch (Exception e) {
+        }
+    }
+
 
     private boolean opened = false;
 
     public void sendCmd(Cmd cmd) {
         if (cmd == null) return;
-        writeLock.lock();
+        try {
+            writeLock.lock();
+        } catch (Exception e) {
+
+        }
         if (cmd.isClearQueue()) {
             cmdQueue.clear();
         }
         cmdQueue.add(cmd);
-        writeCondition.signalAll();
-        writeLock.unlock();
+        try {
+            writeCondition.signalAll();
+        } catch (Exception e) {
+
+        }
+        try {
+            writeLock.unlock();
+        } catch (Exception e) {
+        }
+    }
+
+    private void submitConsumeData(ConsumeData consumeData) {
+        if (consumeData == null) return;
+        if ((!consumeData.isExitSignal()) && stopConsume) return;
+        try {
+            consumeLock.lock();
+        } catch (Exception e) {
+
+        }
+        if (consumeData.isExitSignal()) {
+            consumeDatas.clear();
+        }
+        consumeDatas.add(consumeData);
+        try {
+            consumeCondition.signalAll();
+        } catch (Exception e) {
+
+        }
+        try {
+            consumeLock.unlock();
+        } catch (Exception e) {
+
+        }
     }
 
 
@@ -89,35 +163,27 @@ public class SerialPortHelper {
         public void run() {
             super.run();
             try {
-                while (true) {
-                    readPool.discardReadBytes();
-                    while (!stopRead) {
-                        int readLength = 0;
-                        if (mFileInputStream.available() > 0) {
-                            readLength = mFileInputStream.read(buffer);
-                        }
-                        if (readLength > 0) {
-                            Log.e(TAG, "SerialPortHelper: <<<" + Utils.bytesToHex(buffer, 0, readLength));
-                            readPool.writeBytes(buffer, 0, readLength);
-                            break;//读到数据了，将数据回调出去
-                        } else {
-                            readLock.lock();
-                            if (!stopRead && !skipReadWait) {
-                                readCondition.await(10, TimeUnit.MILLISECONDS);
-                            }
-                            skipReadWait = false;
-                            readLock.unlock();
-                        }
+                while (!stopRead) {
+                    int readLength = 0;
+                    if (mFileInputStream.available() > 0) {
+                        readLength = mFileInputStream.read(buffer);
                     }
-                    if (stopRead) break;
-
-                    try {
-                        callback.receiveData(readPool);
-                    } catch (Exception e) {
-
+                    if (readLength > 0) {
+                        Log.e(TAG, "SerialPortHelper: <<<" + Utils.bytesToHex(buffer, 0, readLength));
+                        byte[] data = new byte[readLength];
+                        System.arraycopy(buffer, 0, data, 0, data.length);
+                        submitConsumeData(new ConsumeData.Builder()
+                                .data(data)
+                                .exitSignal(false)
+                                .build());
+                    } else {
+                        readLock.lock();
+                        if (!stopRead && !skipReadWait) {
+                            readCondition.await(10, TimeUnit.MILLISECONDS);
+                        }
+                        skipReadWait = false;
+                        readLock.unlock();
                     }
-
-
                 }
             } catch (Exception e) {
 
@@ -155,8 +221,9 @@ public class SerialPortHelper {
                     try {
                         if (cmdToSend != null && cmdToSend.getData() != null && cmdToSend.getData().readableBytes() > 0) {
                             readLock.lock();
-                            bytesToSend = new byte[cmdToSend.getData().readableBytes()];
-                            cmdToSend.getData().readBytes(bytesToSend);
+                            ByteBuf slice = cmdToSend.getData().slice();
+                            bytesToSend = new byte[slice.readableBytes()];
+                            slice.readBytes(bytesToSend);
                             mFileOutputStream.write(bytesToSend);
                             skipReadWait = true;
                             readCondition.signalAll();
@@ -199,6 +266,48 @@ public class SerialPortHelper {
             } catch (Exception e) {
 
             }
+        }
+
+    }
+
+    class ConsumeThread extends Thread {
+        @Override
+        public void run() {
+            super.run();
+            try {
+                while (true) {
+                    consumeLock.lock();
+                    if (consumeDatas.isEmpty()) {
+                        consumeCondition.await();
+                        consumeLock.unlock();
+                        continue;
+                    } else {
+                        readPool.discardReadBytes();
+                        while (!consumeDatas.isEmpty()) {
+                            ConsumeData consumeData = consumeDatas.poll();
+                            if (consumeData.isExitSignal()) break;
+                            readPool.writeBytes(consumeData.data);
+                        }
+                    }
+                    consumeLock.unlock();
+                    if (stopConsume) {
+                        break;
+                    }
+                    try {
+                        callback.receiveData(readPool);
+                    } catch (Exception e) {
+
+                    }
+                }
+            } catch (Exception e) {
+
+            }
+            try {
+                consumeLock.unlock();
+            } catch (Exception e) {
+
+            }
+
         }
 
     }
@@ -253,12 +362,19 @@ public class SerialPortHelper {
         mFileOutputStream = mSerialPort.getOutputStream();
         readThread = new ReadThread();
         writeThread = new WriteThread();
+        consumeThread = new ConsumeThread();
         readThread.start();
         writeThread.start();
+        consumeThread.start();
     }
 
 
     private void _close() {
+        try {
+            stopConsume();
+        } catch (Exception e) {
+
+        }
         try {
             stopRead();
         } catch (Exception e) {
@@ -274,6 +390,12 @@ public class SerialPortHelper {
         } catch (Exception e) {
 
         }
+        try {
+            consumeThread.join();
+        } catch (Exception e) {
+
+        }
+
         try {
             readThread.join();
         } catch (Exception e) {
@@ -406,4 +528,56 @@ public class SerialPortHelper {
             return bytesToHex(bytes, 0, bytes.length);
         }
     }
+
+    public static class ConsumeData {
+        byte[] data;
+        boolean exitSignal;
+
+        public ConsumeData() {
+        }
+
+        private ConsumeData(Builder builder) {
+            setData(builder.data);
+            setExitSignal(builder.exitSignal);
+        }
+
+        public byte[] getData() {
+            return data;
+        }
+
+        public void setData(byte[] data) {
+            this.data = data;
+        }
+
+        public boolean isExitSignal() {
+            return exitSignal;
+        }
+
+        public void setExitSignal(boolean exitSignal) {
+            this.exitSignal = exitSignal;
+        }
+
+        public static final class Builder {
+            private byte[] data;
+            private boolean exitSignal;
+
+            public Builder() {
+            }
+
+            public Builder data(byte[] val) {
+                data = val;
+                return this;
+            }
+
+            public Builder exitSignal(boolean val) {
+                exitSignal = val;
+                return this;
+            }
+
+            public ConsumeData build() {
+                return new ConsumeData(this);
+            }
+        }
+    }
+
 }
